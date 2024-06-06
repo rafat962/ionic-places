be2565c3bc0627a48c531d9f86ad80c4160bd21a
rafat962 <rafatkamel96@gmail.com>
2024-06-06 12:08:22 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'places',
+  webDir: 'www'
+};
+
+export default config;
